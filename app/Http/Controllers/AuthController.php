3c88d9336eef1ac9077bbd622944e8ed9a0e7e37
Amir Kouchaki <amirkouchaki1@gmail.com>
2023-02-06 00:28:39 +0330

    created functioning admin authentication in the backend

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required','email'],
+            'password' => ['required'],
+            'remember' => ['boolean']
+        ]);
+        $remember = $credentials['remember'];
+        unset($credentials['remember']);
+
+        if(!Auth::attempt($credentials,$remember))
+            return response(['message' => 'Email or password is wrong'],422);
+
+        /** @var User $user */
+        $user = \auth()->user();
+
+        if(!$user->is_admin){
+            Auth::logout();
+            return response(['message' => 'You don\'t have permission to authenticate as admin'],403);
+        }
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response([
+            'user' => new UserResource($user),
+            'token' => $token
+        ]);
+    }
+
+    public function logout(){
+        /** @var User $user */
+        $user = \auth()->user();
+        $user->currentAccessToken()->delete();
+        return response('',204);
+    }
+
+    public function getUser(Request $request){
+        return new UserResource($request->user());
+    }
+}
---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+            'email_verified_at' => now(),
+            'is_admin' => true
+        ]);
+    }
+}
