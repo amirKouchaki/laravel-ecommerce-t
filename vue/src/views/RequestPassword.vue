19538a1d2449ee12af3015d740936a13b0fa6a32
Amir Kouchaki <amirkouchaki1@gmail.com>
2023-02-05 23:58:56 +0330

    added app and guest layouts and their corresponding pages to router and views

--- vue/src/views/RequestPassword.vue
+++ vue/src/views/RequestPassword.vue
@@ -1,6 +1,6 @@
 <template>
-    <guest-layout title="Sign in to your account ">
-        <form class="mt-8 space-y-6" method="POST" @submit.prevent="login">
+    <guest-layout title="Request new password">
+        <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
             <div class="-space-y-px rounded-md shadow-sm">
                 <div>
@@ -17,40 +17,14 @@
                         placeholder="Email address"
                     />
                 </div>
-                <div>
-                    <label for="password" class="sr-only">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autocomplete="current-password"
-                        required=""
-                        class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Password"
-                    />
-                </div>
             </div>
 
             <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                    <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label
-                        for="remember-me"
-                        class="ml-2 block text-sm text-gray-900"
-                        >Remember me</label
-                    >
-                </div>
-
                 <div class="text-sm">
                     <router-link
-                        :to="{ name: 'requestPassword' }"
+                        :to="{ name: 'login' }"
                         class="font-medium text-indigo-600 hover:text-indigo-500"
-                        >Forgot your password?</router-link
+                        >Remember your password?</router-link
                     >
                 </div>
             </div>
@@ -68,7 +42,7 @@
                             aria-hidden="true"
                         />
                     </span>
-                    Sign in
+                    Submit
                 </button>
             </div>
         </form>
@@ -78,8 +52,6 @@
 <script setup>
 import { LockClosedIcon } from "@heroicons/vue/solid";
 import GuestLayout from "../components/GuestLayout.vue";
-
-const login = () => console.log("login");
 </script>
 
 <style scoped></style>
